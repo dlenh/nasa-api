5cef402cc5953f0903e79150fa3b865c52d947dc
David Lenh <dlenh425@gmail.com>
2023-01-06 12:18:14 -0800

    Commented code based on different media types

--- script.js
+++ script.js
@@ -12,15 +12,15 @@ const input = document.querySelector("input").value
 fetch(`https://api.nasa.gov/planetary/apod?api_key=PqGLdW4QMcIbHfeKzoWglyELNy9lWs74EY5TlsqJ&date=${input}`)
     .then(res => res.json())
     .then(data => {
-        console.log(data)
-        console.log(input)
+        console.log(data) // shows data object from nasa api
+        console.log(input) // shows date user entered
         if (data.code === 400) {
-            document.querySelector(".media").innerText = data.msg
+            document.querySelector(".media").innerText = data.msg // data msg tells user to input valid date
         } else if (data.media_type === "video") {
-            document.querySelector("iframe").src = data.url
+            document.querySelector("iframe").src = data.url // source for when media type is video
             document.querySelector(".description").innerText = data.explanation
         } else if (data.media_type === "image") {
-            document.querySelector("img").src = data.hdurl
+            document.querySelector("img").src = data.hdurl // source for when media type is image
             document.querySelector(".description").innerText = data.explanation
         }   
     })
