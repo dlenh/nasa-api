395f9eebd211f34a9c211cfdbae3dff035507627
David Lenh <dlenh425@gmail.com>
2023-02-13 16:03:32 -0800

    Cleaned up code by deleting repeating code

--- js/script.js
+++ js/script.js
@@ -20,15 +20,14 @@ fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${input}`)
             document.querySelector(".media").innerText = data.msg // data msg tells user to input valid date
         } else if (data.media_type === "video") {
             document.querySelector("iframe").src = data.url // source for when media type is video
-            document.querySelector(".description").innerText = data.explanation
             document.querySelector("iframe").classList.remove("hidden")
             document.querySelector("img").classList.add("hidden")
         } else if (data.media_type === "image") {
             document.querySelector("img").src = data.hdurl // source for when media type is image
-            document.querySelector(".description").innerText = data.explanation
             document.querySelector("img").classList.remove("hidden")
             document.querySelector("iframe").classList.add("hidden")
         }
+        document.querySelector(".description").innerText = data.explanation
         document.querySelector(".media").innerText = `APOD Title: ${data.title}`
     })
     .catch(err => {
